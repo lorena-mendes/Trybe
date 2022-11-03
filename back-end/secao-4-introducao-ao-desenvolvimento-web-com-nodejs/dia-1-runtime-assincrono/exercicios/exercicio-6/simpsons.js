1d59886e41e1414f9c2acf7b38416aba6f9f29bb
Lorena Mendes <lorena.lccma@gmail.com>
2022-11-03 09:10:28 -0300

    Exercicio 6-B

--- back-end/secao-4-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-runtime-assincrono/exercicios/exercicio-6/simpsons.js
+++ back-end/secao-4-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-runtime-assincrono/exercicios/exercicio-6/simpsons.js
@@ -8,8 +8,19 @@ async function readAll() {
   strings.forEach((string) => console.log(string));
 }
 
+async function getSimpsonByID(id) {
+  const fileContent = await fs.readFile('./simpsons.json', 'utf-8') 
+  const simpsons = JSON.parse(fileContent);
+  const idSimpson = simpsons.find((simpson) => Number(simpson.id) === id);
+  if(!idSimpson){
+    throw new Error('id não encontrado')
+  }
+  return idSimpson
+}
+
 function main() {
-  readAll();
+  // readAll();
+  getSimpsonByID(1).then((simpson) => console.log(simpson));
 }
 
 main();

130ce0e822e94e2e9b80e67de670d557db262e2e
lorena-mendes <lorena.lccma@gmail.com>
2022-11-25 15:05:31 -0300

    Iniciei os exercicios do dia 6.1

---
+++ back-end/secao-6-orm-e-autenticacao/exercicios-6.1/src/migrations/20221125111141-create-books.js
@@ -0,0 +1,39 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('Books', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      author: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      pageQuantity: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      }
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('Books');
+  }
+};

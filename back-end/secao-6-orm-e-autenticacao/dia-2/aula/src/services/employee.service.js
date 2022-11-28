a915ea9e32cc0880bceff0ac3b0eaf21ecaae1cd
lorena-mendes <lorena.lccma@gmail.com>
2022-11-28 17:29:22 -0300

    Configuraçãoes da aula em andamento

---
+++ back-end/secao-6-orm-e-autenticacao/dia-2/aula/src/services/employee.service.js
@@ -0,0 +1,26 @@
+
+const { Address, Employee } = require('../models/');
+
+const getAll = async () => {
+  const users = await Employee.findAll({
+    include: { model: Address, as: 'addresses' },
+  });
+
+  return users;
+};
+
+const getById = async (id) => {
+  const employee = await Employee.findOne({
+      where: { id },
+      // include: [{ model: Address, as: 'addresses' }], com o número aparecendo 
+      include: [{
+        model: Address, as: 'addresses', attributes: { exclude: ['number'] }, //sem aparecer o número
+      }],
+    });
+  return employee;
+}
+
+module.exports = {
+  getAll,
+  getById,
+};

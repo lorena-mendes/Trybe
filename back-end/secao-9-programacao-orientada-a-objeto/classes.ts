fdb9a82baf58d25096c44d4f233a184c02e02f76
lorena-mendes <lorena.lccma@gmail.com>
2023-01-11 08:00:55 -0300

    Segunda parte da aula, exercicios para fixar

--- back-end/secao-9-programacao-orientada-a-objeto/classes.ts
+++ back-end/secao-9-programacao-orientada-a-objeto/classes.ts
@@ -1,24 +1,41 @@
 class Tv {
-  brand: string;
-  size: number;
-  resolution: string;
-  connections: string[];
-  connectedTo?: string;
+  private _brand: string;
+  private _size: number;
+  private _resolution: string;
+  private _connections: string[];
+  private _connectedTo?: string;
 
   constructor(b: string, s: number, r: string, c: string[]) {
     console.log(`Creating tv ${b}`);
-    this.brand = b;
-    this.size = s;
-    this.resolution = r;
-    this.connections = c;
+    this._brand = b;
+    this._size = s;
+    this._resolution = r;
+    this._connections = c;
   }
 
   turnOn () {
-    console.log(`${this.brand}, ${this.size}, resolution: ${this.resolution} \n\
-    available connections: ${this.connections}`);    
+    console.log(`${this._brand}, ${this._size}, resolution: ${this._resolution} \n\
+    available connections: ${this._connections}`);    
+  }
+
+  get connectedToGetter() {
+    return this._connectedTo;
+  }
+  
+  set connectedToSetter(newValue: string) {
+    if (!newValue || this._connections.includes(newValue)) {
+      this._connectedTo = newValue;
+      console.log(this._connectedTo);      
+    } else {
+      console.log('Sorry, connection unavailable!');
+      
+    }
   }
 }
 
 const tv1 = new Tv('LG', 50, '4K', ['Bluetooth, Wi-Fi']);
 
 tv1.turnOn(); 
+
+tv1.connectedToSetter = 'Wi-Fi';
+console.log('Connected to: ', tv1.connectedToSetter);

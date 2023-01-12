9c610e509268343b5305a328f451bb9522645e81
lorena-mendes <lorena.lccma@gmail.com>
2023-01-12 15:59:15 -0300

    Primeira parte dos exercícios

---
+++ back-end/secao-9-programacao-orientada-a-objeto/dia-2/aula/index.ts
@@ -0,0 +1,27 @@
+class Superclass {
+  public isSuper: boolean;
+
+  constructor() {
+    this.isSuper = true;    
+  }
+
+  protected sayHello(): void {
+    console.log('Olá mundo!');    
+  }
+}
+
+class Subclass extends Superclass {
+  public sayHello2(): void {
+    this.sayHello();    
+  }
+}
+
+const myFunc = (object: Subclass) => {
+  object.sayHello2();
+}
+
+// const sup = new Superclass();
+const sub = new Subclass();
+
+// myFunc(sup);
+myFunc(sub);

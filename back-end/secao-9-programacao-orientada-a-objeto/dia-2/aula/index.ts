5db31ec12a4bf3311cd75a34866dad7231349865
lorena-mendes <lorena.lccma@gmail.com>
2023-01-12 19:40:09 -0300

    Exercicios da aula (para fixar) finalizados

--- back-end/secao-9-programacao-orientada-a-objeto/dia-2/aula/index.ts
+++ back-end/secao-9-programacao-orientada-a-objeto/dia-2/aula/index.ts
@@ -1,30 +1,49 @@
-class Superclass {
-  public isSuper: boolean;
+// class Superclass {
+//   public isSuper: boolean;
 
-  constructor() {
-    this.isSuper = true;    
-  }
+//   constructor() {
+//     this.isSuper = true;    
+//   }
 
-  public sayHello(): void {
-    console.log('Olá mundo!');    
-  }
-}
+//   public sayHello(): void {
+//     console.log('Olá mundo!');    
+//   }
+// }
 
-class Subclass extends Superclass {
-  constructor() {
-    super();
-    this.isSuper = false;
-  }
-}
+// class Subclass extends Superclass {
+//   constructor() {
+//     super();
+//     this.isSuper = false;
+//   }
+// }
 
-const myFunc = (object: Superclass) => {
-  object.sayHello();
-  console.log(object.isSuper ? 'Super!' : 'Sub!');
+// const myFunc = (object: Superclass) => {
+//   object.sayHello();
+//   console.log(object.isSuper ? 'Super!' : 'Sub!');
   
+// }
+
+// const sup = new Superclass();
+// const sub = new Subclass();
+
+// myFunc(sup);
+// myFunc(sub);
+
+interface MyInterface {
+  myNumber: number;
+
+  myFunc(myParam: number): string;
 }
 
-const sup = new Superclass();
-const sub = new Subclass();
+class MyClass implements MyInterface {
+  constructor(public myNumber: number) {}
 
-myFunc(sup);
-myFunc(sub);
+  myFunc(myParam: number): string {
+    return `myNumber + myParam: ${this.myNumber + myParam}`;
+  }
+}
+
+const object = new MyClass(2);
+console.log(object.myNumber);
+console.log(object.myFunc(4));
+

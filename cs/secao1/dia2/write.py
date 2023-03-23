adf9ba8abaec46d9b1eb2a6a8ee2118a3326c3b1
lorena-mendes <lorena.lccma@gmail.com>
2023-03-23 12:09:00 -0300

    Acompanhamento da aula - manipulação de arquivos

---
+++ cs/secao1/dia2/write.py
@@ -0,0 +1,40 @@
+# file = open("arquivo.txt", mode="w")
+
+# file.write("nome idade\n")  # \n é para quebra de linha
+# file.write("Maria 45\n")
+# file.write("Miguel 33\n")
+
+# # Não precisa da quebra de linha, esse é um comportamento padrão do print
+# print("Túlio 22", file=file)
+
+# LINES = ["Alberto 35\n", "Betina 22\n", "João 42\n", "Victor 19\n"]
+# file.writelines(LINES)
+
+# file.close()
+
+# # escrita
+# file = open("arquivo.txt", mode="w")
+# LINES = ["Olá\n", "mundo\n", "belo\n", "do\n", "Python\n"]
+# file.writelines(LINES)
+# file.close()
+
+# # leitura
+# file = open("arquivo.txt", mode="r")
+# for line in file:
+#     print(
+#         line
+#     )  # não esqueça que a quebra de linha também é um caractere da linha
+# file.close()  # não podemos esquecer de fechar o arquivo
+
+# escrita
+file = open("arquivo.dat", mode="wb")
+file.write(
+    b"C\xc3\xa1ssio 30"
+)  # o prefixo b em uma string indica que seu valor está codificado em bytes
+file.close()
+
+# leitura
+file = open("arquivo.dat", mode="rb")
+content = file.read()
+print(content)  # saída: b'C\xc3\xa1ssio 30'
+file.close()  # não podemos esquecer de fechar o arquivo
